dfa15800660abbb65a3a782a30b090bdffe5767b
Izza Nadeem <izza.nadeem99@gmail.com>
2018-05-21 16:25:46 -0400

    added AR functions, created global var node and used that to keep track of current node and remove it everytime it detects a new horizontal or vertical surface

--- passionProject/ViewController.swift
+++ passionProject/ViewController.swift
@@ -18,4 +18,209 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     var nodeDetected: SCNNode?
       var imageLink: URLS?
     
-   
+    func loadImage() {
+        ImageAPI.manager.getImage { (error, url) in
+            if let error = error {
+                print(error)
+            }
+            else if let url = url {
+                self.imageLink = url
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadImage()
+        guard ARWorldTrackingConfiguration.isSupported else {
+            fatalError("""
+                ARKit is not available on this device. For apps that require ARKit
+                for core functionality, use the `arkit` key in the key in the
+                `UIRequiredDeviceCapabilities` section of the Info.plist to prevent
+                the app from installing. (If the app can't be installed, this error
+                can't be triggered in a production scenario.)
+                In apps where AR is an additive feature, use `isSupported` to
+                determine whether to show UI for launching AR experiences.
+            """) // For details, see https://developer.apple.com/documentation/arkit
+            
+        }
+        
+        //    override func viewDidLoad() {
+        //         super.viewDidLoad()
+        //
+        //        }
+        
+        // Start the view's AR session with a configuration that uses the rear camera,
+        // device position and orientation tracking, and plane detection.
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        sceneView.session.run(configuration)
+        
+        // Set a delegate to track the number of plane anchors for providing UI feedback.
+        sceneView.session.delegate = self
+        
+        // Prevent the screen from being dimmed after a while as users will likely
+        // have long periods of interaction without touching the screen or buttons.
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        //        // Create a new scene
+        //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //        ImageHelper.manager.loadImage(urlString: (imageLink?.small)!) { (error, image) in
+        //            if let image
+        //something like let scene = image
+        //        }
+        //
+        //        // Set the scene to the view
+        //        sceneView.scene = scene
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's AR session.
+        sceneView.session.pause()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+        //        ImageHelper.manager.loadImage(urlString: movie.artworkUrl60) { (error, image) in
+        //            cell.imageView?.image = image
+        //        }
+    }
+    /// - Tag: PlaceARContent
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Place content only for anchors found by plane detection.
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        if nodeDetected != nil {
+            nodeDetected?.removeFromParentNode()
+        }
+        ImageHelper.manager.loadImage(urlString: (imageLink?.small)!) { (error, image) in
+            if let error = error {
+                print(error)
+            }
+            if let image = image {
+                let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+                plane.materials.first?.diffuse.contents = image
+                let planeNode = SCNNode(geometry: plane)
+                planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+                
+                // `SCNPlane` is vertically oriented in its local coordinate space, so
+                // rotate the plane to match the horizontal orientation of `ARPlaneAnchor`.
+                planeNode.eulerAngles.x = -.pi / 2
+                
+                // Make the plane visualization semitransparent to clearly show real-world placement.
+                planeNode.opacity = 1.0
+                
+                // Add the plane visualization to the ARKit-managed node so that it tracks
+                // changes in the plane anchor as plane estimation continues.
+                planeNode.position = SCNVector3Make(0, 0, -5)
+                node.addChildNode(planeNode)
+                self.nodeDetected = node
+            }
+        }
+        
+        
+    }
+    
+    /// - Tag: UpdateARContent
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // Update content only for plane anchors and nodes matching the setup created in `renderer(_:didAdd:for:)`.
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+        
+        // Plane estimation may shift the center of a plane relative to its anchor's transform.
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+        // Plane estimation may also extend planes, or remove one plane to merge its extent into another.
+        plane.width = CGFloat(planeAnchor.extent.x)
+        plane.height = CGFloat(planeAnchor.extent.z)
+    }
+    
+    // MARK: - ARSessionDelegate
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        guard let frame = session.currentFrame else { return }
+        updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
+    }
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        updateSessionInfoLabel(for: session.currentFrame!, trackingState: camera.trackingState)
+    }
+    
+    // MARK: - ARSessionObserver
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay.
+        sessionInfoLabel.text = "Session was interrupted"
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required.
+        sessionInfoLabel.text = "Session interruption ended"
+        resetTracking()
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user.
+        sessionInfoLabel.text = "Session failed: \(error.localizedDescription)"
+        resetTracking()
+    }
+    
+    // MARK: - Private methods
+    
+    private func updateSessionInfoLabel(for frame: ARFrame, trackingState: ARCamera.TrackingState) {
+        // Update the UI to provide feedback on the state of the AR experience.
+        let message: String
+        
+        switch trackingState {
+        case .normal where frame.anchors.isEmpty:
+            // No planes detected; provide instructions for this app's AR interactions.
+            message = "Move the device around to detect horizontal surfaces."
+            
+        case .notAvailable:
+            message = "Tracking unavailable."
+            
+        case .limited(.excessiveMotion):
+            message = "Tracking limited - Move the device more slowly."
+            
+        case .limited(.insufficientFeatures):
+            message = "Tracking limited - Point the device at an area with visible surface detail, or improve lighting conditions."
+            
+        case .limited(.initializing):
+            message = "Initializing AR session."
+            
+        default:
+            // No feedback needed when tracking is normal and planes are visible.
+            // (Nor when in unreachable limited-tracking states.)
+            message = ""
+            
+        }
+        
+        sessionInfoLabel.text = message
+        sessionInfoView.isHidden = message.isEmpty
+    }
+    
+    private func resetTracking() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+}
+
+
